6a4f99d89cf9dc3fa6ac3d04dbe9dfea98ceaf6c
Ricardo Elias Assis da Silva <reas63@hotmail.com>
2026-03-30 22:11:37 -0300

    Create page.tsx

---
+++ app/studio/page.tsx
@@ -0,0 +1,13 @@
+import GeminiBox from "@/components/GeminiBox";
+
+export default function Studio() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">
+        UP AI Studio
+      </h1>
+
+      <GeminiBox />
+    </div>
+  );
+}
