24a253b373f89f5985cc8f7bd5b45e964379db4f
Ricardo Elias Assis da Silva <reas63@hotmail.com>
2026-03-30 23:40:10 -0300

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,9 +4,6 @@ export default function Header() {
       <h1 className="text-4xl font-bold text-red-500">
         UP
       </h1>
-      <p className="text-sm text-gray-500">
-        Digital Art Marketplace
-      </p>
     </div>
   );
 }
