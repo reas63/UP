04b450f4d7e52dd7e10dc545734ef159e1028d64
Ricardo Elias Assis da Silva <reas63@hotmail.com>
2026-03-30 22:09:21 -0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+import Feed from "@/components/Feed";
+
+export default function Home() {
+  return (
+    <div className="p-4">
+      <Header />
+      <Feed />
+    </div>
+  );
+}
