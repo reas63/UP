3d23d26be9309c34c731f1c9334a20ce4744783b
Ricardo Elias Assis da Silva <reas63@hotmail.com>
2026-03-30 22:23:35 -0300

    Create Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <div className="text-center mb-6">
+      <h1 className="text-4xl font-bold text-red-500">
+        UP
+      </h1>
+      <p className="text-sm text-gray-500">
+        Digital Art Marketplace
+      </p>
+    </div>
+  );
+}
