0a1198b42ec93546b281351151609f4eb078eec6
Ricardo Elias Assis da Silva <reas63@hotmail.com>
2026-04-02 02:36:52 -0300

    Update login.tsx

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,24 +4,29 @@ import { supabase } from "../utils/supabaseClient";
 export default function Login() {
   const [email, setEmail] = useState("");
 
-  const handleLogin = async () => {
-    const { error } = await supabase.auth.signInWithOtp({
+  async function login() {
+    await supabase.auth.signInWithOtp({
       email,
     });
 
-    if (error) alert(error.message);
-    else alert("Verifique seu email para login!");
-  };
+    alert("Verifique seu email");
+  }
 
   return (
-    <div className="p-4">
+    <div style={{ padding: 20 }}>
       <h1>Login</h1>
+
       <input
         type="email"
         placeholder="Seu email"
         onChange={(e) => setEmail(e.target.value)}
       />
-      <button onClick={handleLogin}>Entrar</button>
+
+      <br /><br />
+
+      <button onClick={login}>
+        Entrar
+      </button>
     </div>
   );
 }
