5d3093e7e218c38f00a54ec47f4db8e4e06623f0
Ricardo Elias Assis da Silva <reas63@hotmail.com>
2026-04-03 02:43:54 -0300

    Delete src/pages/api/gemini.ts

--- src/pages/api/gemini.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({
-    url: "https://via.placeholder.com/300",
-  });
-}
