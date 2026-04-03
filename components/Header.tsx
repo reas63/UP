d35c785868c42f347ad2f65e58ddb779ff1f9f73
Ricardo Elias Assis da Silva <reas63@hotmail.com>
2026-04-03 12:26:06 -0300

    Delete components directory

--- components/Header.tsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  return (
-    <div className="text-center mb-6">
-      <h1 className="text-4xl font-bold text-red-500">
-        UP
-      </h1>
-    </div>
-  );
-}
